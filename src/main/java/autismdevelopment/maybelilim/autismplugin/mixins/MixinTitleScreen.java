34312900496ea76f308af7ee6e488f76ce1a1111
neverlosedebug <neverloseneverpaste@email.com>
2026-02-15 03:33:40 +0300

    MixinTitleScreen and fixes

---
+++ src/main/java/autismdevelopment/maybelilim/autismplugin/mixins/MixinTitleScreen.java
@@ -0,0 +1,33 @@
+package autismdevelopment.maybelilim.autismplugin.mixins;
+
+import autismdevelopment.maybelilim.autismplugin.client.AutismPluginClient;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.network.chat.Component;
+import namidevelopment.kiriyaga.api.NamiApi;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class MixinTitleScreen {
+
+    @Inject(method = "render", at = @At("RETURN"))
+    private void renderWatermark(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
+        Minecraft mc = Minecraft.getInstance();
+        if (mc.font == null) return;
+
+        String name = AutismPluginClient.PLUGIN_NAME;
+        String version = AutismPluginClient.VERSION;
+
+        String formatted = "{red}" + name + "{/}{gray} - {/}{white}" + version + "{/}";
+        Component textComponent = NamiApi.CAT_FORMAT.format(formatted);
+
+        int x = 2;
+        int y = 2;
+
+        guiGraphics.drawString(mc.font, textComponent, x, y, -1, true);
+    }
+}
